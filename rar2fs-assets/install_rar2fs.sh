7ace2ddea0f92dd0f09cece47d29228fb3356e4d
Erik Tagesson <49564549+eccoes@users.noreply.github.com>
2023-07-24 19:53:22 +0200

    Initial commit

---
+++ rar2fs-assets/install_rar2fs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /tmp
+git clone https://github.com/hasse69/rar2fs.git
+cd rar2fs
+wget https://www.rarlab.com/rar/unrarsrc-6.0.7.tar.gz
+tar -xvzf unrarsrc-6.0.7.tar.gz
+cd unrar
+make lib
+make install-lib
+cd ..
+autoreconf -f -i
+./configure && make
+make install
+cd /
