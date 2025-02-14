ecc374803fbf6bc4aac04dd59fbce8e945d387fe
Erik Tagesson <49564549+eccoes@users.noreply.github.com>
2025-02-14 16:00:41 +0100

    Update install_rar2fs.sh

--- rar2fs-assets/install_rar2fs.sh
+++ rar2fs-assets/install_rar2fs.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
 cd /tmp
-wget https://www.rarlab.com/rar/unrarsrc-7.1.3.tar.gz
-tar -xvzf unrarsrc-7.1.3.tar.gz
+git clone https://github.com/eccoes/rar2fs.git
+cd rar2fs
+wget https://www.rarlab.com/rar/unrarsrc-6.0.7.tar.gz
+tar -xvzf unrarsrc-6.0.7.tar.gz
 cd unrar
 make lib
 make install-lib
-make clean
+cd ..
+autoreconf -f -i
 ./configure && make
 make install
 cd /
-
-git clone https://github.com/hasse69/rar2fs.git -C /tmp/rar2fs
-cd /tmp/rar2fs
-autoreconf -f -i
-./configure --with-unrar=/tmp/unrar
-make 
-make install
