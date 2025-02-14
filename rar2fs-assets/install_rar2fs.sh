47554bfe641aca175fad60ad81c3884cc3357cd4
Erik Tagesson <49564549+eccoes@users.noreply.github.com>
2025-02-14 15:54:09 +0100

    Update install_rar2fs.sh

--- rar2fs-assets/install_rar2fs.sh
+++ rar2fs-assets/install_rar2fs.sh
@@ -1,16 +1,19 @@
-#!/bin/sh 
-wget https://www.rarlab.com/rar/unrarsrc-7.0.8.tar.gz -P /tmp/
-tar -xvzf /tmp/unrarsrc-7.0.8.tar.gz -C /tmp
-make lib -j$(($(nproc) + 1)) -l$(($(nproc) + 2)) -C /tmp/unrar
-make install-lib -C /tmp/unrar -f /tmp/unrar/makefile
-make clean -j$(($(nproc) + 1)) -l$(($(nproc) + 2)) -C /tmp/unrar -f /tmp/unrar/makefile
-make -j$(($(nproc) + 1)) -l$(($(nproc) + 2)) -C /tmp/unrar -f /tmp/unrar/makefile
-make install -C /tmp/unrar
-rm -rf -v /tmp/unrar{-7.0.8.tar.gz}
+#!/bin/sh
 
-git clone https://github.com/hasse69/rar2fs.git /tmp/rar2fs
+cd /tmp
+wget https://www.rarlab.com/rar/unrarsrc-7.1.3.tar.gz
+tar -xvzf unrarsrc-7.1.3.tar.gz
+cd unrar
+make lib
+make install-lib
+make clean
+./configure && make
+make install
+cd /
+
+git clone https://github.com/hasse69/rar2fs.git -C /tmp/rar2fs
 cd /tmp/rar2fs
 autoreconf -f -i
-./configure
-make -j$(($(nproc)+1)) -l$(($(nproc)+2)) -C /tmp/rar2fs
-make install -C /tmp/rar2fs
+./configure --with-unrar=/tmp/unrar
+make 
+make install
