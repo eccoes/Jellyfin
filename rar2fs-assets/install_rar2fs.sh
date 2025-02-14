b7b14a79853e0e5f86aa04a5bb9e60d9a0eff13f
Erik Tagesson <49564549+eccoes@users.noreply.github.com>
2025-02-14 15:33:17 +0100

    Update install_rar2fs.sh

--- rar2fs-assets/install_rar2fs.sh
+++ rar2fs-assets/install_rar2fs.sh
@@ -1,16 +1,9 @@
-#!/bin/sh
-
-cd /tmp
-git clone https://github.com/hasse69/rar2fs.git
-cd rar2fs
-wget https://www.rarlab.com/rar/unrarsrc-7.1.3.tar.gz
-tar -xvzf unrarsrc-7.1.3.tar.gz
-cd unrar
-make lib
-make install-lib
-make clean
-cd ..
-autoreconf -f -i
-./configure && make
-make install
-cd /
+#!/bin/sh 
+wget https://www.rarlab.com/rar/unrarsrc-7.0.8.tar.gz -P /tmp/
+tar -xvzf /tmp/unrarsrc-7.0.8.tar.gz -C /tmp
+make lib -j$(($(nproc) + 1)) -l$(($(nproc) + 2)) -C /tmp/unrar
+make install-lib -C /tmp/unrar -f /tmp/unrar/makefile
+make clean -j$(($(nproc) + 1)) -l$(($(nproc) + 2)) -C /tmp/unrar -f /tmp/unrar/makefile
+make -j$(($(nproc) + 1)) -l$(($(nproc) + 2)) -C /tmp/unrar -f /tmp/unrar/makefile
+make install -C /tmp/unrar
+rm -rf -v /tmp/unrar{-7.0.8.tar.gz}
